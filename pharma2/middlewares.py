256b2e7c016f900d633a9ee6f0d3a93c4d467e91
Ankit Dsouza <dsouzaankit@gmail.com>
2017-11-04 23:41:44 -0400

    Update middlewares.py

--- pharma2/middlewares.py
+++ pharma2/middlewares.py
@@ -1,1 +1,5 @@
-
+#Custom middleware must be after Redirect middleware in priority
+DOWNLOADER_MIDDLEWARES = {
+    'pharma2.middlewares.RandomUserAgentMiddleware': 80,
+    'pharma2.middlewares.CustomMiddleware': 650,
+}
