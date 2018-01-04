70329c7abf37fefda91f0ad6b17f6b85ad8bb629
Ankit Dsouza <dsouzaankit@gmail.com>
2018-01-04 16:54:53 -0500

    Update middlewares.py

--- pharma2/middlewares.py
+++ pharma2/middlewares.py
@@ -1,59 +1,78 @@
-import scrapy
+import random
 from scrapy import signals
 from scrapy.conf import settings
-import random
-from time import sleep
+from stem import Signal
+from stem.control import Controller
+#from time import sleep
+
+
+def _set_new_ip():
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate(password='tor_password')
+        controller.signal(Signal.NEWNYM)
+
 
 class RandomUserAgentMiddleware(object):
     def process_request(self, request, spider):
-        ua  = random.choice(settings.get('USER_AGENT_LIST'))
+        ua = random.choice(settings.get('USER_AGENT_LIST'))
         if ua:
             request.headers.setdefault('User-Agent', ua)
 
-class CustomMiddleware(object):
 
+class RandomProxy(object):
+    # overwrite process request
+    def process_request(self, request, spider):
+        # Set location of proxy
+        p = random.choice(settings.get('PROXY_LIST'))
+        if p:
+            request.meta['proxy'] = p
+
+
+class SleepRetryMiddleware(object):
     no_of_requests = 0
     proxy_index = 0
 
     # overwrite process request
     def process_request(self, request, spider):
 
-        #print(request.url)
+        # print(request.url)
         if 'checkspammer' in request.url:
-            #bypass captcha by supplying required form data in a POST request
+            # bypass captcha by supplying required form data in a POST request
             orig_url = request.url.split('fromurl=')[1]
-            request = request.replace(url = orig_url)
+            request = request.replace(url=orig_url)
+            request.dont_filter = True
             print("Anti-bot redirection!!")
-            request = scrapy.FormRequest(
-                orig_url,
-                formdata = {'scode': '657515',
-                          'code': '657515',
-                          'validated': 'Yes',
-                          'fromurl': orig_url},
-                meta = {'item': request.meta['item']},
-                dont_filter = True)
+            self.proxy_index += 1
+            if self.proxy_index % len(settings.get('PROXY_LIST')) == 0:
+                self.proxy_index = 0
+            return request
 
         if 'dr_fail' in request.url:
-            #bypass IP blocked message by changing proxy
+            # bypass IP blocked message by changing proxy
             orig_url = request.meta.get('redirect_urls', [request.url])[0]
             print("IP limit reached!! Changing IP")
-            request = request.replace(url = orig_url)
+            # _set_new_ip()
+            request = request.replace(url=orig_url)
             request.dont_filter = True
             self.proxy_index += 1
             if self.proxy_index % len(settings.get('PROXY_LIST')) == 0:
-                print("IPs exhausted!! Pause for some time")
-                sleep(301)
-            return request
+                self.proxy_index = 0
+                #     print("IPs exhausted!! Pause for some time")
+                #     sleep(301)
+        return request
 
-        print('Proxy# ' + str(self.proxy_index % len(settings.get('PROXY_LIST'))))
+        # print('Proxy# ' + str(self.proxy_index % len(settings.get('PROXY_LIST'))))
         # Set a proxy from list
         request.meta['proxy'] = settings.get('PROXY_LIST')[self.proxy_index % len(settings.get('PROXY_LIST'))]
+
+        # send via privoxy
+        # request.meta['proxy'] = 'http://127.0.0.1:8118'	
 
     # overwrite process response
     def process_response(self, request, response, spider):
 
+        #	abc = spider.crawler.stats 
         self.no_of_requests += 1
         print("Response# " + str(self.no_of_requests))
 
         return response
-        
